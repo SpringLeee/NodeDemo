43d025e6f6b1eda2db9c37592b9d90e6b60670b4
SpringLee <1028789852@qq.com>
2017-06-21 11:24:23 +0800

    提交

---
+++ 启动服务/server.js
@@ -0,0 +1,14 @@
+﻿var http = require("http")
+
+http.createServer(function (request,response) {
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    response.end('hello node');
+
+}).listen(8888);
+
+console.log("8888 启动完成！")
+
+
+

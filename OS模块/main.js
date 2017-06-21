a0d1e1f283cff519a4cb0dd01cfd27e3a724e1be
SpringLee <1028789852@qq.com>
2017-06-21 14:47:11 +0800

    本地提交

---
+++ OS模块/main.js
@@ -0,0 +1,9 @@
+﻿
+var os = require("os")
+console.log(os.hostname())
+console.log(os.platform())
+console.log(os.type())
+console.log(os.totalmem())
+console.log(os.freemem())
+console.log(os.cpus())
+console.log(os.networkInterfaces())

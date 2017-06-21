a4a87b6fcb25d9e632fe266726d4a1a48709e8cc
SpringLee <1028789852@qq.com>
2017-06-21 14:25:43 +0800

    http

---
+++ Http/server.js
@@ -0,0 +1,18 @@
+﻿var http = require("http")
+var url = require("url")
+var util = require("util")
+
+
+http.createServer(function (req, res) {
+
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.end("lallla");
+
+    var data = url.parse(req.url, true).query
+
+    console.log(data)
+
+}).listen(4444);
+
---
+++ 文件系统/main.js
@@ -0,0 +1,7 @@
+﻿var fs = require("fs")
+//fs.writeFile("dd.txt", "呵呵");
+
+
+
+//删除文件
+fs.unlink("dd.txt");

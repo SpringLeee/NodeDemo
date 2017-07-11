c7e6091e8f4685d64fafeb8d82d417a220ce5b88
SpringLee <1028789852@qq.com>
2017-07-11 15:00:35 +0800

    提交

--- Http/server.js
+++ Http/server.js
@@ -1,18 +1,15 @@
-﻿var http = require("http")
-var url = require("url")
-var util = require("util")
+﻿var http = require('http');
 
+http.createServer(function (request, response) {
 
-http.createServer(function (req, res) {
+	// 发送 HTTP 头部 
+	// HTTP 状态值: 200 : OK
+	// 内容类型: text/plain
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 
+	// 发送响应数据 "Hello World"
+	response.end('Hello World\n');
+}).listen(8888);
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    res.end("lallla");
-
-    var data = url.parse(req.url, true).query
-
-    console.log(data)
-
-}).listen(4444);
-
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');

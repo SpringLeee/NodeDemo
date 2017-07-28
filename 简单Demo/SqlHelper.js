ee790d6e9e1d6d768c5f4201235ad2440354053a
SpringLee <1028789852@qq.com>
2017-07-28 10:05:04 +0800

    submit

--- 简单Demo/SqlHelper.js
+++ 简单Demo/SqlHelper.js
@@ -5,7 +5,6 @@ var conn = mysql.createConnection({
     host: 'rxcpt009.mysql.rds.aliyuncs.com',
     user: 'rxcpt01',
     password: 'hercules4c4c4c',
-
     database: 'testcodefirst'
 
 });
@@ -29,6 +28,7 @@ db.Commit = function (sql, strs, fun) {
     conn.query(sql, strs, function (err,result,fields) {
 
         fun(err, result, fields)
+        
 
     })
 
